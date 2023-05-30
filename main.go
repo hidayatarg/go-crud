3c7b4faafec230f837265bc6925e613592a9f054
Hidayat Arghandabi <hidayatarg@gmail.com>
2023-05-30 16:40:32 +0300

    Created First Gin WebServer

--- main.go
+++ main.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	fmt.Println("Hello Go")
 	fmt.Println("It is working")
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
