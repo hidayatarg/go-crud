6c12ee66111e96f4f393d781f9d06822eaa1512a
Hidayat Arghandabi <hidayatarg@gmail.com>
2023-05-30 16:45:00 +0300

    Added Dotenv Variable

--- main.go
+++ main.go
@@ -2,13 +2,27 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
+
+// init -> run before main
+
+func init() {
+	// load environment variables
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading environment variables")
+	}
+
+}
 
 func main() {
 	fmt.Println("Hello Go")
 	fmt.Println("It is working")
+
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
